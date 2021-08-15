770908743143a9ac7e5b2387f603747bc32f08cf
Karen Xu <karenyrxu@gmail.com>
2021-08-15 18:31:34 -0400

    clean extra

--- sample-apps/java-agent-aws-sdk-terraform/variables.tf
+++ sample-apps/java-agent-aws-sdk-terraform/variables.tf
@@ -6,8 +6,6 @@ variable "function_name" {
 
 variable "path_to_custom_collector_config_zip" {
   type        = string
-  description = "File path to the zipped custom collector config.yaml file. Leave as empty to use the default config (without an AMP endpoint)"
-  default     = "../../integration-tests/amp-terraform/custom-config-layer.zip" 
-  // default = null
-  // remove default after finish testing
+  description = "File path to the zipped custom collector config.yaml file. Leave as default to use the default collector config (without AMP)"
+  default = null 
 }
