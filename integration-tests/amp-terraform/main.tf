64103199b0ec92eeabbadab2b9a351f66497cc9b
Karen Xu <karenyrxu@gmail.com>
2021-08-15 14:29:21 -0400

    build replace.sh into terraform file

--- integration-tests/amp-terraform/main.tf
+++ integration-tests/amp-terraform/main.tf
@@ -1,5 +1,10 @@
+# set up an AMP workspace used only for integration tests (assume customer already has their own AMP workspace endpoint)
 data "aws_region" "current" {}
 
-resource "aws_prometheus_workspace" "lambda-test" {
-    alias = "lambda-test"
+resource "aws_prometheus_workspace" "test_amp_workspace" {
+
+    // create a config.yaml file with the AMP "endpoint" and "region" 
+    provisioner "local-exec" {
+        command = "sed 's@<remote_write_endpoint>@${self.prometheus_endpoint}api/v1/remote_write@g; s@<workspace_region>@${data.aws_region.current.name}@g' preformatted_config.yaml > config.yaml; zip custom-config-layer.zip config.yaml;"
+    }
 }
--- integration-tests/amp-terraform/outputs.tf
+++ integration-tests/amp-terraform/outputs.tf
@@ -1,11 +1,4 @@
+# used by CI 
 output "amp_endpoint" {
-  value = aws_prometheus_workspace.lambda-test.prometheus_endpoint
-}
-
-output "remotewrite_endpoint" {
-  value = "${aws_prometheus_workspace.lambda-test.prometheus_endpoint}api/v1/remote_write"
-}
-
-output "aws_region" {
-  value = data.aws_region.current.name
+  value = aws_prometheus_workspace.test_amp_workspace.prometheus_endpoint
 }
