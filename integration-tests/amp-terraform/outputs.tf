69e928d4f77bd638229e3eebb2811d1704949273
Karen Xu <karenyrxu@gmail.com>
2021-08-15 22:45:33 -0400

    remove amp-terraform directory

--- integration-tests/amp-terraform/outputs.tf
+++
@@ -1,4 +0,0 @@
-# used by CI 
-output "amp_endpoint" {
-  value = aws_prometheus_workspace.test_amp_workspace.prometheus_endpoint
-}
--- integration-tests/java/aws-sdk/agent/main.tf
+++ integration-tests/java/aws-sdk/agent/main.tf
@@ -1,3 +1,5 @@
+data "aws_region" "current" {}
+
 module "test" {
   source = "../../../../opentelemetry-lambda/java/integration-tests/aws-sdk/agent"
 
@@ -6,6 +8,16 @@ module "test" {
   function_name          = var.function_name
   collector_config_layer_arn = aws_lambda_layer_version.collector_config_layer.arn
   tracing_mode = "Active"
+}
+
+
+# set up an AMP workspace used only for integration tests (assume customer already has their own AMP workspace endpoint)
+resource "aws_prometheus_workspace" "test_amp_workspace" {
+
+    // create a config.yaml file with the AMP "endpoint" and "region", and zips it
+    provisioner "local-exec" {
+        command = "sed 's@<remote_write_endpoint>@${self.prometheus_endpoint}api/v1/remote_write@g; s@<workspace_region>@${data.aws_region.current.name}@g' preformatted_config.yaml > config.yaml; zip custom-config-layer.zip config.yaml; rm config.yaml; "
+    }
 }
 
 resource "aws_iam_role_policy_attachment" "test_xray" {
@@ -20,9 +32,8 @@ resource "aws_iam_role_policy_attachment" "test_amp" {
 
 resource "aws_lambda_layer_version" "collector_config_layer" {
   layer_name          = "custom_collector_config"
-  filename            = "${path.module}/../../../amp-terraform/custom-config-layer.zip"
+  filename            = "${path.module}/custom-config-layer.zip"
   compatible_runtimes = ["java8", "java8.al2", "java11"]
   license_info        = "Apache-2.0"
-  source_code_hash    = filebase64sha256("${path.module}/../../../amp-terraform/custom-config-layer.zip")
+  source_code_hash    = filebase64sha256("${path.module}/custom-config-layer.zip")
 }
-
