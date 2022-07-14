df70fa602c1b9b01256b50e66df71c2c1f6b7cf5
Pavan Sai Vasireddy (Vasi) <pvasir@amazon.com>
2022-07-14 16:20:09 -0700

    testing

--- nodejs/integration-tests/aws-sdk/wrapper/variables.tf
+++ nodejs/integration-tests/aws-sdk/wrapper/variables.tf
@@ -21,3 +21,15 @@ variable "architecture" {
   description = "Lambda function architecture, either arm64 or x86_64"
   default     = "x86_64"
 }
+
+variable "use_existing_cloudwatch_log_group" {
+  description = "Whether to use an existing CloudWatch log group or create new"
+  type        = bool
+  default     = false
+}
+
+variable "cloudwatch_logs_retention_in_days" {
+  description = "Specifies the number of days you want to retain log events in the specified log group. Possible values are: 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, and 3653."
+  type        = number
+  default     = 2
+}
